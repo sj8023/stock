a84f3d6e313970898b114ea27f4ea5ba055b6d47
sj8023 <913691377@qq.com>
2016-12-04 14:36:18 +0000

    Delete get_xinlang.pl

--- get_xinlang.pl
+++
@@ -1,7 +0,0 @@
-In [1]: import pandas 
-In [2]: import pandas.io.data
-
-In [3]: sym = "BABA"
-
-In [4]: finace = pandas.io.data.DataReader(sym, "yahoo", start="2014/11/11")
-In [5]: print finace.tail(3)
