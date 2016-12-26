91bf4c6dabc64b0add2122af55ca98c93ec85250
sj8023 <913691377@qq.com>
2016-12-26 15:57:15 +0800

    Update Perl 腾讯股票信息收集.pl

--- Perl 腾讯股票信息收集.pl
+++ Perl 腾讯股票信息收集.pl
@@ -66,10 +66,6 @@ GetStocks();
 @result = sort { ($a->[ORDER] || 0) <=> ($b->[ORDER] || 0) } @result; #可以使用grep添加更多过滤条件
 PrintStock();
 
-
-=head
-Get stock information from QQ
-=cut
 sub GetStockInfomation{
 	return unless @_;
 	
@@ -122,79 +118,3 @@ sub PrintStock{
 
 	print "nTotal: ",$#result;
 }
-
-输出：
-
-
-
-    上一个：Perl的错误处理（1）下一个：[Perl]用log4perl记录日志 
-
-用户评论
-登录
-我的社区
-有事没事说两句...
-
-    评论
-
-1人参与,1条评论
-
-    最新评论
-
-2016年7月23日 7:43 宣璇亲
-
-我只看看
-回复
-查看更多
-ThinkSAAS正在使用畅言
-去社区看看吧
-
-    Python之多线程_python_ThinkSAAS
-    Python之多线程_python_ThinkSAAS
-    Python网络数据采集系列-------概述_python_ThinkSAAS
-    Python网络数据采集系列-------概述_python_ThinkSAAS
-    抢哪个？诺基亚Lumia920/820/720/620/520合影_开源资讯_ThinkSAAS
-    抢哪个？诺基亚Lumia920/820/720/620/520合影_开源资讯_ThinkSAAS
-    log4php0.9的详细配置实例说明_PHP教程_ThinkSAAS
-    log4php0.9的详细配置实例说明_PHP教程_ThinkSAAS
-
-    Python之多线程_python_ThinkSAAS
-    Python网络数据采集系列-------概述_python_ThinkSAAS
-    抢哪个？诺基亚Lumia920/820/720/620/520合影_开源资讯_ThinkSAAS
-    log4php0.9的详细配置实例说明_PHP教程_ThinkSAAS
-
-热评话题
-
-    log4php0.9的详细配置实例说明_PHP教程_ThinkSAAS
-    Linux系统Apache用户授权和拜访把持_PHP教程_ThinkSAAS
-    撑不住了，新浪微博支持分享到微信和来往_热点聚合_ThinkSAAS
-    jQuery TagBox下载和文档_开源软件_ThinkSAAS
-    内存：C/C++编程的重要概念_C++,C语言_ThinkSAAS
-    jquery fx分析_Jquery_ThinkSAAS
-    SpringMVC+Hibernate+Spring整合（二）_Java_ThinkSAAS
-
-perl学习最新帖子
-
-    Perl生成隨機密碼
-    Perl写的查单词利器
-    在window平台下自动截屏
-    $& $` $' 格式匹配
-    Perl 菜鸟解决"易语言难题”  
-    kickstart file generate tool use cgi
-    动态生成pxelinux菜单文件及kickstart安装脚本
-    单体评价手顺书生成工具0.1版
-    postfix邮件日志分析工具pflogsumm 分析结果导入数据库
-    perl子进程给父进程传数据
-
-perl学习热门帖子
-
-    Perl使用Tesseract-OCR实现验证码识别教程
-    Perl内置特殊变量总结
-    Perl中的特殊内置变量详细介绍
-    perl实现的两个文件对比并对数据进行筛选的脚本代码
-    perl哈希的一个实例分析
-    perl用{}修饰变量名的写法分享
-    Linux/Unix下安装Perl模块的两种方法分享
-    perl ping检测功能脚本代码
-    Perl操作系统环境变量的脚本代码
-    Perl中处理时间的几个函数
-
